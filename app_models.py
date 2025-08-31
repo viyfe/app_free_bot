43eb8d89accd5b37931b193d561126747262ef94
viyfe <lianviy3095@gmail.com>
2025-08-31 16:23:05 +0800

    Update app_models.py

--- app_models.py
+++ app_models.py
@@ -15,3 +15,13 @@ class PushHistory(db.Model):
     app_id = db.Column(db.Integer, db.ForeignKey('app_info.id'))
     channel = db.Column(db.String(50))
     pushed_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+class AppInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    desc = db.Column(db.Text)
+    link = db.Column(db.String(300))
+    src = db.Column(db.String(50))
+    icon_url = db.Column(db.String(300))  # 新增：图标链接
+    pushed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)

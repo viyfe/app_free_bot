f5c76a9d8d537fd3b5af01f79273921acb9b8a52
viyfe <lianviy3095@gmail.com>
2025-08-31 16:23:29 +0800

    Update app_models.py

--- app_models.py
+++ app_models.py
@@ -1,20 +1,5 @@
 from . import db
 from datetime import datetime
-
-class AppInfo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    desc = db.Column(db.Text)
-    link = db.Column(db.String(300))
-    src = db.Column(db.String(50))
-    pushed = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-class PushHistory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    app_id = db.Column(db.Integer, db.ForeignKey('app_info.id'))
-    channel = db.Column(db.String(50))
-    pushed_at = db.Column(db.DateTime, default=datetime.utcnow)
     
 class AppInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,3 +10,9 @@ class AppInfo(db.Model):
     icon_url = db.Column(db.String(300))  # 新增：图标链接
     pushed = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class PushHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    app_id = db.Column(db.Integer, db.ForeignKey('app_info.id'))
+    channel = db.Column(db.String(50))
+    pushed_at = db.Column(db.DateTime, default=datetime.utcnow)
